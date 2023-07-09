17d73bf0e333fb9d26aa1468842736671e4750cb
Nasir <nasir5074@gmail.com>
2023-07-09 23:38:22 +0400

    changes

--- zz_module/cart/add_to_cart.php
+++ zz_module/cart/add_to_cart.php
@@ -19,10 +19,21 @@ $cart_product_data = $general->getAll($cart_product_qry);
 if( is_array($cart_product_data) && count($cart_product_data) > 0 ){
 
 }else{
-	//$product_data = $general->getAll("product_prd WHERE id_prd =".$product_id);
+	$product_data = $general->CustomQuery("SELECT color_id,sku,name_prd,thumb_name_prd,price_prd,sale_price,
+																				 (SELECT name FROM colors WHERE id = product_prd.color_id ) AS product_color,
+																				 (SELECT name FROM size_details WHERE id = product_prd.size_id) AS product_size
+																			 FROM product_prd WHERE id_prd =".$product_id);
+	$product_data = $product_data[0];
+	// echo "<pre>"; print_r($product_data); die;
+	if( $product_data['sale_price'] > 0 ){
+		$price = $product_data['sale_price'];
+	}else{
+		$price = $product_data['price_prd'];
+	}
 	//echo "<pre>"; print_r($product_data);
-	$cart_product_insert        = "cart_id, product_id, size_id, color_id, product_sku, product_name, product_color, product_size, product_quantity, product_price";
-	$cart_product_insert_values = "$cart_id,$product_id,$product_size_id";
+	$cart_product_insert        = "cart_id, product_id, size_id, color_id, product_sku, product_name,product_image,product_color, product_size, product_quantity, product_price";
+	$cart_product_insert_values = "$cart_id,$product_id,$product_size_id,".$product_data['color_id'].",'".$product_data['sku']."','".$product_data['name_prd']."','".$product_data['thumb_name_prd']."','".$product_data['product_color']."','".$product_data['product_size']."','$product_quantity','".$price."'";
+	$general->InsertRecord('cart_details',$cart_product_insert,$cart_product_insert_values);
 }
 
 ?>
